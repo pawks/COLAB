285c6f257beee2d16e32fbb961a964005e29f865
S.Pawan Kumar <spawan1999@gmail.com>
2018-01-26 19:47:20 +0530

    4 and 5 working perfectly

--- assignment1/test1.s
+++ assignment1/test1.s
@@ -1,17 +1,5 @@
-ldr r0,=filename
-mov r1,#0
-swi 0x66
-ldr r1,=filehandle
-str r0,[r1]
-ldr r1,=text
-mov r2,#1
-swi 0x6a
-ldr r0,=text
-swi 0x00
-ldr r0,=filehandle
-ldr r0,[r0]
-swi 0x68
+mov r0,#0x01
+swi 0x201
+mov r0,#0
+swi 0x201
 swi 0x11
-filehandle: .skip 4
-text: .skip 1
-filename: .asciz "input.txt"
